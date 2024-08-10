79667ac44bcd58de5e64ae593cbaf2723ed66cc5
nis-sin <nischalgurungji@gmail.com>
2024-08-10 23:06:06 +0100

    Changed firebase path

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import {useState, useEffect} from "react";
-import {firestore} from "../firebase";
+import {firestore} from "/firebase";
 import {Box, Modal, Typography, Stack, TextField, Button} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import IconButton from '@mui/material/IconButton';
